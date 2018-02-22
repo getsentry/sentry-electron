2635aec885392037b645055f7d3347389bce0630
Jan Michael Auer <jan.auer@sentry.io>
2018-02-22 09:24:50 +0100

    feat: Add an example

---
+++ example/main.js
@@ -0,0 +1,36 @@
+// Activate the Sentry Electron SDK as early as possible in every process.
+// To support errors in renderer processes on Linux and Windows, make sure
+// to include this line in those files as well.
+require('./sentry');
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const url = require('url');
+
+app.on('ready', () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden',
+  });
+
+  window.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true,
+    }),
+  );
+});
+
+app.on('window-all-closed', () => app.quit());
+
+// The IPC handlers below trigger errors in the here (main process) when
+// the user clicks on corresponding buttons in the UI (renderer).
+ipcMain.on('demo.error', () => {
+  throw new Error('Error triggered in main processes');
+});
+
+ipcMain.on('demo.crash', () => {
+  process.crash();
+});
