dc5b3cdeae6b2998943be4f7d9732730c1583b03
Daniel Griesser <daniel.griesser.86@gmail.com>
2018-02-26 15:47:43 +0100

    feat: Add tests with spectron (#19)
    * feat: Add tests with spectron
    
    * feat: Add xvfb
    
    * fix: Add yarn install
    
    * fix: Change install script
    
    * feat: Add appdata dir
    
    * fix: Build app before tests, Add xvfb-maybe
    
    * fix: Increase mocha async timeout
    
    * feat: Add throw renderer error

--- example/sentry.js
+++ example/sentry.js
@@ -2,7 +2,8 @@ const Sentry = require('@sentry/core');
 const { SentryElectron } = require('..');
 
 // TODO: Add your DSN here
-const MY_DSN = '';
+const MY_DSN =
+  'https://37f8a2ee37c0409d8970bc7559c7c7e4:4cfde0ca506c4ea39b4e25b61a1ff1c3@sentry.io/277345';
 
 Sentry.create(MY_DSN)
   .use(SentryElectron)
