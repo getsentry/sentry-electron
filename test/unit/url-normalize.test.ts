76b011acb0c066f6613e4813d0a28317d64bb3bf
Tim Fish <tim@timfishy.co.uk>
2018-02-28 23:14:17 +0100

    Fix the tests

--- test/unit/url-normalize.test.ts
+++ test/unit/url-normalize.test.ts
@@ -13,25 +13,25 @@ describe('Normalize URLs', () => {
     const base = 'c:/Users/Username/sentry-electron/example';
 
     expect(normalizeUrl('C:\\Users\\Username\\sentry-electron\\example\\renderer.js', base))
-      .to.equal('app://renderer.js');
+      .to.equal('app:///renderer.js');
 
     expect(normalizeUrl('C:\\Users\\Username\\sentry-electron\\example\\sub-directory\\renderer.js', base))
-      .to.equal('app://sub-directory/renderer.js');
+      .to.equal('app:///sub-directory/renderer.js');
 
     expect(normalizeUrl('file:///C:/Users/Username/sentry-electron/example/index.html', base))
-      .to.equal('app://index.html');
+      .to.equal('app:///index.html');
   });
 
   it('Asar packaged app in Windows Program Files', () => {
     const base = 'C:/Program Files/My App/resources/app.asar';
 
     expect(normalizeUrl('/C:/Program%20Files/My%20App/resources/app.asar/dist/bundle-app.js', base))
-      .to.equal('app://dist/bundle-app.js');
+      .to.equal('app:///dist/bundle-app.js');
 
     expect(normalizeUrl('file:///C:/Program%20Files/My%20App/resources/app.asar/index.html', base))
-      .to.equal('app://index.html');
+      .to.equal('app:///index.html');
 
     expect(normalizeUrl('file:///C:/Program%20Files/My%20App/resources/app.asar/a/index.html', base))
-      .to.equal('app://a/index.html');
+      .to.equal('app:///a/index.html');
   });
 });
