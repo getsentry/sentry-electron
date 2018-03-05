78b2e565891fcde4c6d2e480acfa8df13fa4f180
Tim Fish <tim@timfishy.co.uk>
2018-03-05 13:10:27 +0000

    test: Add more tests (#28)
    * Add more tests
    
    * option to stop clearing userdata between app instances
    
    * Lint failing on CI

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -8,7 +8,7 @@ let context: TestContext;
 
 use(chaiAsPromised);
 
-describe('Test', () => {
+describe('Basic Tests', () => {
   beforeEach(async () => {
     context = new TestContext();
     await context.start();
@@ -18,32 +18,73 @@ describe('Test', () => {
     await context.stop();
   });
 
-  it('Throw renderer error', async () => {
-    await context.app.client
-      .waitForExist('#error-render')
-      .click('#error-render');
+  it('JavaScript exception in renderer process', async () => {
+    await context.clickCrashButton('#error-render');
 
     await context.waitForTrue(() => context.testServer.events.length >= 1);
 
     expect(context.testServer.events.length).to.equal(1);
     expect(context.testServer.events[0].dump_file).to.equal(undefined);
-    expect(context.testServer.events[0].sentry_key).to.equal('37f8a2ee37c0409d8970bc7559c7c7e4');
-    expect(context.testServer.events[0].data.culprit).to.equal('app:///renderer.js');
+    expect(context.testServer.events[0].sentry_key).to.equal(
+      '37f8a2ee37c0409d8970bc7559c7c7e4',
+    );
+    expect(context.testServer.events[0].data.culprit).to.equal(
+      'app:///renderer.js',
+    );
+    expect(context.testServer.events[0].data.breadcrumbs.length).to.greaterThan(
+      5,
+    );
   });
 
-  it('Crash renderer', async () => {
-    try {
-      await context.app.client
-        .waitForExist('#crash-render')
-        .click('#crash-render');
-    } catch (e) {
-      // The renderer crashes and causes an exception in 'click'
-    }
+  it('JavaScript exception in main process', async () => {
+    await context.clickCrashButton('#error-main');
+
+    await context.waitForTrue(() => context.testServer.events.length >= 1);
+
+    expect(context.testServer.events.length).to.equal(1);
+    expect(context.testServer.events[0].dump_file).to.equal(undefined);
+    expect(context.testServer.events[0].sentry_key).to.equal(
+      '37f8a2ee37c0409d8970bc7559c7c7e4',
+    );
+    expect(context.testServer.events[0].data.culprit).to.equal(
+      'main at EventEmitter.ipcMain.on',
+    );
+    expect(context.testServer.events[0].data.breadcrumbs.length).to.greaterThan(
+      5,
+    );
+  });
+
+  it('Native crash in renderer process', async () => {
+    await context.clickCrashButton('#crash-render');
 
     await context.waitForTrue(() => context.testServer.events.length >= 1);
 
     expect(context.testServer.events.length).to.equal(1);
     expect(context.testServer.events[0].dump_file).to.be.instanceOf(Buffer);
-    expect(context.testServer.events[0].sentry_key).to.equal('37f8a2ee37c0409d8970bc7559c7c7e4');
+    expect(context.testServer.events[0].sentry_key).to.equal(
+      '37f8a2ee37c0409d8970bc7559c7c7e4',
+    );
+    expect(context.testServer.events[0].data.breadcrumbs.length).to.greaterThan(
+      5,
+    );
+  });
+
+  it('Native crash in main process', async () => {
+    await context.clickCrashButton('#crash-main');
+
+    // We have to restart the app to send native crashes from the main process
+    await context.stop(false);
+    await context.start();
+
+    await context.waitForTrue(() => context.testServer.events.length >= 1);
+
+    expect(context.testServer.events.length).to.equal(1);
+    expect(context.testServer.events[0].dump_file).to.be.instanceOf(Buffer);
+    expect(context.testServer.events[0].sentry_key).to.equal(
+      '37f8a2ee37c0409d8970bc7559c7c7e4',
+    );
+    expect(context.testServer.events[0].data.breadcrumbs.length).to.greaterThan(
+      5,
+    );
   });
 });
--- test/e2e/test-context.ts
+++ test/e2e/test-context.ts
@@ -37,8 +37,12 @@ export class TestContext {
       this.testServer.start();
     }
 
-    // Get a temp directory for this app to use as userData
-    this.tempDir = await this.getTempDir();
+    // Only setup the tempDir if this the first start of the context
+    // Subseqent starts will se the same path
+    if (!this.tempDir) {
+      // Get a temp directory for this app to use as userData
+      this.tempDir = await this.getTempDir();
+    }
 
     this.app = new Application({
       path: this.electronPath,
@@ -46,7 +50,7 @@ export class TestContext {
       env: {
         DSN:
           'http://37f8a2ee37c0409d8970bc7559c7c7e4:4cfde0ca506c4ea39b4e25b61a1ff1c3@localhost:8000/277345',
-        E2E_APPDATA_DIRECTORY: this.tempDir.path,
+        E2E_USERDATA_DIRECTORY: this.tempDir.path,
       },
     });
 
@@ -61,7 +65,7 @@ export class TestContext {
   }
 
   /** Stops the app and cleans up  */
-  public async stop(): Promise<void> {
+  public async stop(clearData: boolean = true): Promise<void> {
     try {
       if (this.app && this.app.isRunning()) {
         await this.app.stop();
@@ -70,7 +74,7 @@ export class TestContext {
       // When the app crashes we can lose the session
     }
 
-    if (this.tempDir) {
+    if (this.tempDir && clearData) {
       this.tempDir.cleanup();
     }
 
@@ -80,6 +84,14 @@ export class TestContext {
 
     await this.mainProcess.kill();
     await this.tryKillChromeDriver();
+  }
+
+  public async clickCrashButton(selector: string): Promise<void> {
+    try {
+      await this.app.client.waitForExist(selector).click(selector);
+    } catch (e) {
+      // If the renderer crashes it can cause an exception in 'click'
+    }
   }
 
   /**
@@ -106,7 +118,7 @@ export class TestContext {
    * When the renderer crashes, Chromedriver does not close and does not respond.
    * We have to find the process and kill it.
    *
-   * @param {number} [pid=process.pid]
+   * @param {number} [pid=process.pid] The root pid to check sub-processes
    */
   private async tryKillChromeDriver(pid: number = process.pid): Promise<void> {
     // @ts-ignore
@@ -114,7 +126,7 @@ export class TestContext {
       if ((each.name as string).toLowerCase().includes('chromedriver')) {
         process.kill(each.pid);
       } else {
-        this.tryKillChromeDriver(each.pid);
+        await this.tryKillChromeDriver(each.pid);
       }
     }
   }
