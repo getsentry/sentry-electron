921e980d85ed7eccd1cd32b5bf08311477311c61
Jan Michael Auer <jan.auer@sentry.io>
2018-03-12 18:02:16 +0100

    ref(example): Use the new API

--- example/sentry.js
+++ example/sentry.js
@@ -1,10 +1,11 @@
-const Sentry = require('@sentry/core');
-const { SentryElectron } = require('..');
+const { SentryClient } = require('..');
 
+// TODO: Replace with your project's DSN
 const MY_DSN =
   process.env.DSN ||
   'https://37f8a2ee37c0409d8970bc7559c7c7e4:4cfde0ca506c4ea39b4e25b61a1ff1c3@sentry.io/277345';
 
-Sentry.create(MY_DSN)
-  .use(SentryElectron)
-  .install();
+SentryClient.create({
+  dsn: MY_DSN,
+  // TODO: Add more options
+});
