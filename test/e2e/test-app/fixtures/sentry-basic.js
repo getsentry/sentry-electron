82477e75b9b13ea0b4e56b54c66494a94068c040
HazA <daniel.griesser.86@gmail.com>
2018-07-16 14:19:01 +0200

    fix: Don't show dialog

--- test/e2e/test-app/fixtures/sentry-basic.js
+++ test/e2e/test-app/fixtures/sentry-basic.js
@@ -2,4 +2,7 @@ const { init } = require('../../../../');
 
 init({
   dsn: process.env.DSN,
+  onFatalError: error => {
+    // We need this here otherwise we will get a dialog and travis will be stuck
+  },
 });
