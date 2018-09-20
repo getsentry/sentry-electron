579bfcd92d21436db0262f0b07d68f026e4ddbbd
HazA <daniel.griesser.86@gmail.com>
2018-09-20 13:26:57 +0200

    feat: Update sdkInformation integration from core

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,17 +1,22 @@
 // tslint:disable-next-line
 require('util.promisify/shim')();
-import { initAndBind } from '@sentry/core';
+import { initAndBind, Integrations as CoreIntegrations } from '@sentry/core';
 import { defaultIntegrations } from '@sentry/node';
 import { ElectronOptions } from '..';
 import { MainClient } from './client';
-import { Electron, OnUncaughtException, SDKInformation } from './integrations';
+import { Electron, OnUncaughtException } from './integrations';
 import { NetTransport } from './transports/net';
 export { MainClient } from './client';
 export { MainBackend } from './backend';
 export { Integrations as NodeIntegrations } from '@sentry/node';
+import { SDK_NAME } from '../sdk';
 
 // tslint:disable-next-line:variable-name
-export const ElectronIntegrations = { Electron, OnUncaughtException, SDKInformation };
+export const ElectronIntegrations = { Electron, OnUncaughtException };
+
+/** SDK version used in every event. */
+// tslint:disable-next-line
+export const SDK_VERSION: string = require('../../package.json').version;
 
 /**
  * Init call to node, if no transport is set, we use net of electron
@@ -30,7 +35,11 @@ export function init(options: ElectronOptions): void {
       // tslint:disable-next-line:no-unbound-method
       new OnUncaughtException({ onFatalError: options.onFatalError }),
       new Electron(),
-      new SDKInformation(),
+      new CoreIntegrations.SDKInformation({
+        name: 'npm:@sentry/electron',
+        sdkName: SDK_NAME,
+        sdkVersion: SDK_VERSION,
+      }),
     ],
   );
 }
