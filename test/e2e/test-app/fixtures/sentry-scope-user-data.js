dcec25c0b322c0b00ef8fcc38a34f89f08504fe6
Chris Russell <crussell@sonnysdirect.com>
2020-01-10 17:55:13 -0500

    Add e2e test for testing user data w/ exception

---
+++ test/e2e/test-app/fixtures/sentry-scope-user-data.js
@@ -0,0 +1,12 @@
+const { init, configureScope } = require('../../../../');
+
+init({
+  dsn: process.env.DSN,
+  onFatalError: error => {
+    // We need this here otherwise we will get a dialog and travis will be stuck
+  },
+});
+
+configureScope((scope) => {
+  scope.setUser({ id: 'johndoe' });
+});
