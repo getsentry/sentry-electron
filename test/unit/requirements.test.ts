cd2a19278193762077e42f37275a1ca908ce9224
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-07-09 15:20:29 +0200

    ref: Add automated test

--- test/unit/requirements.test.ts
+++ test/unit/requirements.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import * as fs from 'fs';
+import { exec } from 'child_process';
 
 describe('TS -> JS conversion check', () => {
   // We should make sure we always use electron.net module since it deals with all possible
@@ -7,5 +8,14 @@ describe('TS -> JS conversion check', () => {
   it('Uses electron.net module', () => {
     const content = fs.readFileSync('dist/main/transports/net.js');
     expect(content.toString()).to.match(/net.request\(/);
+  });
+});
+
+describe('Bundle with webpack', () => {
+  it('should not throw TypeError: mod.require is not a function', done => {
+    exec('cd test/integration/bundle-test-app/; yarn && yarn build && yarn start', (_err, stdout) => {
+      expect(stdout).to.not.match(/TypeError: mod\.require is not a function/);
+      done();
+    });
   });
 });
