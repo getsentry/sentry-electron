d3376e3b349b8bd87b38b6b00cfd2796fdbe1342
Jan Michael Auer <mail@jauer.org>
2020-09-11 16:05:55 +0200

    fix(minidump): Add filenames to attachment items

--- src/main/uploader.ts
+++ src/main/uploader.ts
@@ -212,16 +212,14 @@ export class MinidumpUploader {
     // Remove all metadata files and forget about them.
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     Promise.all(
-      files
-        .filter(file => file.endsWith('.txt') && !file.endsWith('log.txt'))
-        .map(async file => {
-          const path = join(this._crashesDirectory, file);
-          try {
-            await unlinkAsync(path);
-          } catch (e) {
-            logger.warn('Could not delete', path);
-          }
-        }),
+      files.filter(file => file.endsWith('.txt') && !file.endsWith('log.txt')).map(async file => {
+        const path = join(this._crashesDirectory, file);
+        try {
+          await unlinkAsync(path);
+        } catch (e) {
+          logger.warn('Could not delete', path);
+        }
+      }),
     );
 
     return files.filter(file => file.endsWith('.dmp')).map(file => join(this._crashesDirectory, file));
@@ -301,6 +299,7 @@ export class MinidumpUploader {
         attachment_type: 'event.minidump',
         length: minidumpContent.length,
         type: 'attachment',
+        filename: basename(minidumpPath),
       });
 
       bodyBuffer = Buffer.concat([bodyBuffer, Buffer.from(`${minidumpHeader}\n`), minidumpContent, Buffer.from('\n')]);
