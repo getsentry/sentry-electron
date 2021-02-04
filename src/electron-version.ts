ac6eceb086398526f7dce0309160be2c14af7909
Tim Fish <tim@timfish.uk>
2021-02-04 16:40:58 +0000

    Few more changes

--- src/electron-version.ts
+++ src/electron-version.ts
@@ -1,14 +1,19 @@
 import { parseSemver } from '@sentry/utils';
 
-const version = parseSemver(process.versions.electron);
-const major: number = version.major || 0;
-const minor: number = version.minor || 0;
+/**
+ * Parsed Electron version
+ */
+function version(): { major: number; minor: number; patch: number } {
+  const version = parseSemver(process.versions.electron);
+  return { major: version.major || 0, minor: version.minor || 0, patch: version.patch || 0 };
+}
 
 /**
  * Electron >=8.4 | >=9.1 | >=10
  * Use `render-process-gone` rather than `crashed`
  */
 export function supportsRenderProcessGone(): boolean {
+  const { major, minor } = version();
   return (major === 8 && minor >= 4) || (major === 9 && minor >= 1) || major >= 10;
 }
 
@@ -16,6 +21,7 @@ export function supportsRenderProcessGone(): boolean {
  * Electron < 9 requires `crashReporter.start()` in the renderer
  */
 export function requiresNativeHandlerRenderer(): boolean {
+  const { major } = version();
   return major < 9;
 }
 
@@ -23,5 +29,6 @@ export function requiresNativeHandlerRenderer(): boolean {
  * Electron >= 6 uses crashpad on Windows
  */
 export function supportsCrashpadOnWindows(): boolean {
+  const { major } = version();
   return major >= 6;
 }
