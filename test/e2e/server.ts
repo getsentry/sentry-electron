6e628c738b4b25886c1f404b6a0946e6fff61230
Tim Fish <tim@timfish.uk>
2021-03-10 11:53:12 +0000

    Reduce test body limit

--- test/e2e/server.ts
+++ test/e2e/server.ts
@@ -41,7 +41,7 @@ export class TestServer {
         extendTypes: {
           text: ['application/x-sentry-envelope'],
         },
-        textLimit: '200mb',
+        textLimit: '100mb',
       }),
     );
 
