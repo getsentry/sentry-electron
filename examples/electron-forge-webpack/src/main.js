435bb8fc61e7ee58fd30aee9358990dea870f2e3
Tim Fish <tim@timfish.uk>
2021-10-19 19:57:48 +0100

    Fix linting

--- examples/electron-forge-webpack/src/main.js
+++ examples/electron-forge-webpack/src/main.js
@@ -16,6 +16,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     show: false,
     webPreferences: {
+      // eslint-disable-next-line no-undef
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
       nodeIntegration: false,
       contextIsolation: true,
@@ -23,8 +24,8 @@ const createWindow = () => {
     },
   });
 
+  // eslint-disable-next-line no-undef
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-  // mainWindow.webContents.openDevTools();
 };
 
 app.on('ready', createWindow);
