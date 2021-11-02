c2943c95d0dcfc8e838fe2c05ab1da77873961ef
Tim Fish <tim@timfish.uk>
2021-11-02 14:37:25 +0000

    Generate version.ts on build

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,8 +1,6 @@
 const { join } = require('path');
-const { readFileSync, writeFileSync } = require('fs');
+const { writeFileSync } = require('fs');
 
-const version = require('../package.json').version;
 const path = join(__dirname, '../src/main/version.ts');
-const code = readFileSync(path, { encoding: 'utf8' });
-const modified = code.replace(/SDK_VERSION = '[\S\.]*'/, `SDK_VERSION = '${version}'`);
-writeFileSync(path, modified);
+const version = require('../package.json').version;
+writeFileSync(path, `export const SDK_VERSION = '${version}';\n`);
--- src/main/version.ts
+++
@@ -1,1 +0,0 @@
-export const SDK_VERSION = '3.0.0-beta.2';
