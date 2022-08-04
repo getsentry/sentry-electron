48b0f41e51b1fcc884720d994a4700fc0858318c
Tim Fish <tim@timfish.uk>
2022-08-04 17:35:36 +0100

    Submit native crash to Sentry

--- test/e2e/test-apps/native-sentry/renderer/src/main.js
+++ test/e2e/test-apps/native-sentry/renderer/src/main.js
@@ -4,7 +4,7 @@ const { app, BrowserWindow } = require('electron');
 const { init } = require('@sentry/electron');
 
 init({
-  dsn: '__DSN__',
+  dsn: 'https://233a45e5efe34c47a3536797ce15dafa@o447951.ingest.sentry.io/5650507',
   debug: true,
   autoSessionTracking: false,
   onFatalError: () => {},
