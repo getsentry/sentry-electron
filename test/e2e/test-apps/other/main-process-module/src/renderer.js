3064e99c9b84b71f6de7006f477658483df40dc8
Tim Fish <tim@timfish.uk>
2023-07-13 15:21:58 +0100

    fix: Normalize filename before parsing module name (#699)

---
+++ test/e2e/test-apps/other/main-process-module/src/renderer.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-unresolved
+import { init, configureScope } from '@sentry/electron';
+
+init({
+  debug: true,
+});
+
+configureScope((scope) => {
+  scope.setUser({ id: 'abc-123' });
+});
