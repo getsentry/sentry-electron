32695bbd93b9ffc09e4af73dd68a83eba5b05aab
Tim Fish <tim@timfish.uk>
2023-09-13 01:26:09 +0200

    test visibility too

--- src/main/integrations/browser-window-session.ts
+++ src/main/integrations/browser-window-session.ts
@@ -7,7 +7,7 @@ import { endSession, endSessionOnExit, startSession } from '../sessions';
 function focusedWindow(): boolean {
   for (const window of BrowserWindow.getAllWindows()) {
     if (!window.isDestroyed() && window.webContents && !window.webContents.isDestroyed()) {
-      if (window.isFocused() || window.webContents.isDevToolsFocused()) {
+      if (window.isFocused() && window.isVisible()) {
         return true;
       }
     }
