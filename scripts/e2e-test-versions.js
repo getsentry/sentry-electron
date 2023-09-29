b5bbec29694677a6b6b09e9798068909ba29139f
Tim Fish <tim@timfish.uk>
2023-09-29 11:35:16 +0200

    test: Don't test all Electron versions

--- scripts/e2e-test-versions.js
+++ scripts/e2e-test-versions.js
@@ -2,10 +2,5 @@ const { readFileSync } = require('fs');
 
 const versions = JSON.parse(readFileSync('./test/e2e/versions.json', 'utf8'));
 
-if (process.env.GITHUB_REF && process.env.GITHUB_REF.includes('release/')) {
-  // For release builds we test all versions
-  console.log(JSON.stringify(versions));
-} else {
-  // Otherwise we test the oldest version and the last 10 versions
-  console.log(JSON.stringify([versions[0], ...versions.slice(-10)]));
-}
+// We test the oldest version and the last 10 versions
+console.log(JSON.stringify([versions[0], ...versions.slice(-10)]));
