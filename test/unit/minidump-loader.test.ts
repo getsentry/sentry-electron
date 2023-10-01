77b55ec5bec4e0eb80d3ce4f3952ba9a58e3f8dd
Tim Fish <tim@timfish.uk>
2023-10-02 00:48:07 +0200

    also ensure old minidumps are not sent!

--- test/unit/minidump-loader.test.ts
+++ test/unit/minidump-loader.test.ts
@@ -1,6 +1,6 @@
 import { uuid4 } from '@sentry/utils';
 import { expect } from 'chai';
-import { existsSync, writeFileSync } from 'fs';
+import { existsSync, utimesSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import * as tmp from 'tmp';
 
@@ -12,14 +12,15 @@ function dumpFileName(): string {
 
 const VALID_LOOKING_MINIDUMP = Buffer.from(`MDMP${'x'.repeat(12_000)}`);
 const LOOKS_NOTHING_LIKE_A_MINIDUMP = Buffer.from('X'.repeat(12_000));
+const MINIDUMP_HEADER_BUT_TOO_SMALL = Buffer.from('MDMPdflahfalfhalkfnaklsfnalfkn');
 
 describe('createMinidumpLoader', () => {
   let tempDir: tmp.DirResult;
-  beforeEach(() => {
+  before(() => {
     tempDir = tmp.dirSync({ unsafeCleanup: true });
   });
 
-  afterEach(() => {
+  after(() => {
     if (tempDir) {
       tempDir.removeCallback();
     }
@@ -47,36 +48,60 @@ describe('createMinidumpLoader', () => {
   });
 
   it("doesn't send invalid minidumps", (done) => {
-    const dumpPath = join(tempDir.name, dumpFileName());
-    writeFileSync(dumpPath, LOOKS_NOTHING_LIKE_A_MINIDUMP);
+    const missingHeaderDump = join(tempDir.name, dumpFileName());
+    writeFileSync(missingHeaderDump, LOOKS_NOTHING_LIKE_A_MINIDUMP);
+    const tooSmallDump = join(tempDir.name, dumpFileName());
+    writeFileSync(tooSmallDump, MINIDUMP_HEADER_BUT_TOO_SMALL);
 
-    const loader = createMinidumpLoader(() => Promise.resolve([dumpPath]));
+    const loader = createMinidumpLoader(() => Promise.resolve([missingHeaderDump, tooSmallDump]));
 
-    let returnedAttachment = false;
+    let passedAttachment = false;
     void loader(false, () => {
-      returnedAttachment = true;
+      passedAttachment = true;
     });
 
     setTimeout(() => {
-      expect(returnedAttachment).to.be.false;
+      expect(passedAttachment).to.be.false;
+      expect(existsSync(missingHeaderDump)).to.be.false;
+      expect(existsSync(tooSmallDump)).to.be.false;
+      done();
+    }, 2_000);
+  });
+
+  it("doesn't send minidumps that are over 30 days old", (done) => {
+    const dumpPath = join(tempDir.name, dumpFileName());
+    writeFileSync(dumpPath, VALID_LOOKING_MINIDUMP);
+    const now = new Date().getTime() / 1000;
+    const thirtyOneDaysAgo = now - 31 * 24 * 3_600;
+    utimesSync(dumpPath, thirtyOneDaysAgo, thirtyOneDaysAgo);
+
+    const loader = createMinidumpLoader(() => Promise.resolve([dumpPath]));
+
+    let passedAttachment = false;
+    void loader(false, () => {
+      passedAttachment = true;
+    });
+
+    setTimeout(() => {
+      expect(passedAttachment).to.be.false;
       expect(existsSync(dumpPath)).to.be.false;
       done();
     }, 2_000);
   });
 
-  it('deletes minidumps when instructed', (done) => {
+  it('deletes minidumps when sdk is disabled', (done) => {
     const dumpPath = join(tempDir.name, dumpFileName());
     writeFileSync(dumpPath, VALID_LOOKING_MINIDUMP);
 
     const loader = createMinidumpLoader(() => Promise.resolve([dumpPath]));
 
-    let returnedAttachment = false;
+    let passedAttachment = false;
     void loader(true, () => {
-      returnedAttachment = true;
+      passedAttachment = true;
     });
 
     setTimeout(() => {
-      expect(returnedAttachment).to.be.false;
+      expect(passedAttachment).to.be.false;
       expect(existsSync(dumpPath)).to.be.false;
       done();
     }, 2_000);
