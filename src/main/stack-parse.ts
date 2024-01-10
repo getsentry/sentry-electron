616eeb490e8a677252838ace6756318f0401be04
Tim Fish <tim@timfish.uk>
2024-01-10 20:09:43 +0100

    Fix a couple of tests

--- src/main/stack-parse.ts
+++ src/main/stack-parse.ts
@@ -2,8 +2,9 @@ import { createGetModuleFromFilename } from '@sentry/node';
 import { StackParser } from '@sentry/types';
 import { createStackParser, nodeStackLineParser } from '@sentry/utils';
 import { app } from 'electron';
+import { sep } from 'path';
 
 // node.js stack parser but filename normalized before parsing the module
 export const defaultStackParser: StackParser = createStackParser(
-  nodeStackLineParser(createGetModuleFromFilename(app.getAppPath())),
+  nodeStackLineParser(createGetModuleFromFilename(app.getAppPath() + sep)),
 );
