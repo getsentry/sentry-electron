6179f136229ca605adb167468c8a9efe229160fa
Tim Fish <tim@timfish.uk>
2024-02-03 23:41:18 -0400

    Fix test

--- test/e2e/test-apps/other/child-process/src/main.js
+++ test/e2e/test-apps/other/child-process/src/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const { app, BrowserWindow } = require('electron');
-const { init, childProcessIntegration } = require('@sentry/electron');
+const { init, childProcessIntegration } = require('@sentry/electron/main');
 
 init({
   dsn: '__DSN__',
