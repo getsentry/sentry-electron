effcb3997f8a9fa4616d922105ebb20687d22bd6
Tim Fish <tim@timfish.uk>
2024-03-18 17:19:08 +0000

    feat: Limit minidumps sent per app session (#846)

---
+++ test/e2e/test-apps/native-sentry/renderer-limit/src/main.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const { app, BrowserWindow } = require('electron');
+const { init, sentryMinidumpIntegration } = require('@sentry/electron/main');
+
+init({
+  dsn: '__DSN__',
+  debug: true,
+  autoSessionTracking: false,
+  integrations: [sentryMinidumpIntegration({ maxMinidumpsPerSession: 1 })],
+  onFatalError: () => {},
+});
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Keep reloading the window to cause multiple crash events
+  app.on('render-process-gone', () => {
+    console.log('Reloading window');
+    mainWindow.reload();
+  });
+});
