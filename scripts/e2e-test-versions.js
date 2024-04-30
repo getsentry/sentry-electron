5ced72b7f849e6f6a7926699571bb8675a079e99
Tim Fish <tim@timfish.uk>
2024-05-01 00:10:40 +0100

    fix: Don't test v20 in CI on macOS

--- scripts/e2e-test-versions.js
+++ scripts/e2e-test-versions.js
@@ -1,12 +1,17 @@
 const { readFileSync } = require('fs');
 
-const versions = JSON.parse(readFileSync('./test/e2e/versions.json', 'utf8'));
+let versions = JSON.parse(readFileSync('./test/e2e/versions.json', 'utf8'));
+
+// Electron v20 exits immediately on macOS arch64 in GitHub Actions with SIGTRAP
+if (process.env.CI && process.platform === 'darwin') {
+  versions = versions.filter((version) => !version.startsWith('20.'));
+}
 
 if (process.env.GITHUB_REF && process.env.GITHUB_REF.includes('release/')) {
   // For release builds we test all versions
   console.log(JSON.stringify(versions));
 } else {
   const versionCount = process.platform === 'darwin' ? -3 : -7;
-  // Otherwise we test the oldest version and the last 10 versions
+  // Otherwise we test the oldest supported version and the last 3 or 7 versions depending on the platform
   console.log(JSON.stringify([versions[0], ...versions.slice(versionCount)]));
 }
