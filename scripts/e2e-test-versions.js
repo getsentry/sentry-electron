97015d59c480f222a95d3fd68647605c10e882cc
Tim Fish <tim@timfish.uk>
2024-04-30 15:33:32 +0100

    test all versions

--- scripts/e2e-test-versions.js
+++ scripts/e2e-test-versions.js
@@ -2,11 +2,11 @@ const { readFileSync } = require('fs');
 
 const versions = JSON.parse(readFileSync('./test/e2e/versions.json', 'utf8'));
 
-if (process.env.GITHUB_REF && process.env.GITHUB_REF.includes('release/')) {
-  // For release builds we test all versions
-  console.log(JSON.stringify(versions));
-} else {
-  const versionCount = process.platform === 'darwin' ? -3 : -7;
-  // Otherwise we test the oldest version and the last 10 versions
-  console.log(JSON.stringify([versions[0], ...versions.slice(versionCount)]));
-}
+// if (process.env.GITHUB_REF && process.env.GITHUB_REF.includes('release/')) {
+// For release builds we test all versions
+console.log(JSON.stringify(versions));
+// } else {
+//   const versionCount = process.platform === 'darwin' ? -3 : -7;
+//   // Otherwise we test the oldest version and the last 10 versions
+//   console.log(JSON.stringify([versions[0], ...versions.slice(versionCount)]));
+// }
