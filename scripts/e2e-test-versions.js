b6387ca06d9ffe284ae872d2f87aeb7c9c04db0f
Tim Fish <tim@timfish.uk>
2024-05-02 14:16:02 +0100

    debug: Test version matrix code

--- scripts/e2e-test-versions.js
+++ scripts/e2e-test-versions.js
@@ -7,11 +7,11 @@ if (process.platform === 'darwin') {
   versions = versions.filter((version) => !version.startsWith('20.'));
 }
 
-if (process.env.GITHUB_REF && process.env.GITHUB_REF.includes('release/')) {
-  // For release builds we test all versions
-  console.log(JSON.stringify(versions));
-} else {
-  const versionCount = process.platform === 'darwin' ? -3 : -7;
-  // Otherwise we test the oldest supported version and the last 3 or 7 versions depending on the platform
-  console.log(JSON.stringify([versions[0], ...versions.slice(versionCount)]));
-}
+// if (process.env.GITHUB_REF && process.env.GITHUB_REF.includes('release/')) {
+// For release builds we test all versions
+console.log(JSON.stringify(versions));
+// } else {
+//   const versionCount = process.platform === 'darwin' ? -3 : -7;
+//   // Otherwise we test the oldest supported version and the last 3 or 7 versions depending on the platform
+//   console.log(JSON.stringify([versions[0], ...versions.slice(versionCount)]));
+// }
