3b6d3a54cd428e81d49c85ad37287a4a47601822
Tim Fish <tim@timfish.uk>
2025-01-09 20:20:01 +0100

    Add test

---
+++ test/e2e/test-apps/native-sentry/unknown/src/main.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const { writeFileSync } = require('fs');
+
+const { app, BrowserWindow } = require('electron');
+const { init, getCurrentScope } = require('@sentry/electron/main');
+
+const VALID_LOOKING_MINIDUMP = Buffer.from(`MDMP${'X'.repeat(12000)}`);
+
+const minidumpPath = path.join(
+  app.getPath('crashDumps'),
+  process.platform === 'win32' ? 'reports' : 'completed',
+  '0dc9e285-df8d-47b7-8147-85308b54065a.dmp'
+);
+
+init({
+  dsn: '__DSN__',
+  debug: true,
+  autoSessionTracking: false,
+  onFatalError: () => {},
+});
+
+getCurrentScope().setTag('app-run', process.env.APP_FIRST_RUN ? 'first' : 'second');
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  if (process.env.APP_FIRST_RUN) {
+    console.log('main process breadcrumb from first crashing run');
+
+    setTimeout(() => {
+      writeFileSync(minidumpPath, VALID_LOOKING_MINIDUMP);
+
+      setTimeout(() => {
+        process.exit();
+       });
+    }, 2000);
+  } else {
+    console.log('main process breadcrumb from second run');
+  }
+});
