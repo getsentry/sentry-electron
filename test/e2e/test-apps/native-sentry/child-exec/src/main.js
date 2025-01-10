46ef80f70cecc343ddb0e78d510fd6743cc679b8
Tim Fish <tim@timfish.uk>
2025-01-10 03:55:20 +0100

    Test crashing child exec

---
+++ test/e2e/test-apps/native-sentry/child-exec/src/main.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const child_process = require('child_process');
+
+const { getPath } = require('crashy-cli');
+const { app, BrowserWindow } = require('electron');
+const { init } = require('@sentry/electron/main');
+
+init({
+  dsn: '__DSN__',
+  debug: true,
+  autoSessionTracking: false,
+  onFatalError: () => {},
+});
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  if (process.env.APP_FIRST_RUN) {
+    try {
+      child_process.execSync(getPath());
+    } catch (_) {
+      //
+    }
+
+    setTimeout(() => {
+      app.exit();
+    }, 3000);
+  }
+});
