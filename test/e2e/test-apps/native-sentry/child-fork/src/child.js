10c5007b764bfa9566c91fafdd23d44bb0b654e8
Tim Fish <tim@timfish.uk>
2025-01-18 02:39:51 +0000

    start the crashReporter in the child

--- test/e2e/test-apps/native-sentry/child-fork/src/child.js
+++ test/e2e/test-apps/native-sentry/child-fork/src/child.js
@@ -2,6 +2,17 @@ const { raiseSegfault } = require('sadness-generator')
 
 console.log('Child process started', typeof process.crashReporter);
 
+if (process.crashReporter) {
+  process.crashReporter.start({
+    companyName: '',
+    ignoreSystemCrashHandler: true,
+    productName: 'app-name',
+    submitURL: 'https://f.a.k/e',
+    uploadToServer: false,
+    compress: true,
+  });
+}
+
 setTimeout(() => {
   raiseSegfault();
 }, 1000);
