b849c203897304124810d41f5062879a635fd83e
Tim Fish <tim@timfish.uk>
2025-01-21 10:45:44 +0100

    test: More child process types (#1063)

---
+++ test/e2e/test-apps/native-sentry/child-fork/src/main.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const child_process = require('child_process');
+
+const { app, BrowserWindow } = require('electron');
+const { init } = require('@sentry/electron/main');
+
+init({
+  dsn: '__DSN__',
+  debug: true,
+  autoSessionTracking: false,
+  onFatalError: () => {},
+});
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  if (process.env.APP_FIRST_RUN) {
+    child_process.fork(path.join(__dirname, 'child.js'));
+
+    setTimeout(() => {
+      app.exit();
+    }, 3000);
+  }
+});
