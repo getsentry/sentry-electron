38557e65c8400414bc72545ceda62c63aa33cd86
Tim Fish <tim@timfish.uk>
2025-04-30 18:24:14 +0200

    oops

--- test/e2e/test-apps/sessions/abnormal-exit-electron-uploader/test.ts
+++ test/e2e/test-apps/sessions/abnormal-exit-electron-uploader/test.ts
@@ -3,73 +3,78 @@ import { expect } from 'vitest';
 
 import { electronTestRunner, getSessionFromEnvelope, ISO_DATE_MATCHER, sessionEnvelope, UUID_MATCHER } from '../../..';
 
-electronTestRunner(__dirname, { runTwice: true, timeout: 50_000 }, async (ctx) => {
-  let firstSessionId: string | undefined;
+electronTestRunner(
+  __dirname,
+  { runTwice: true, timeout: 50_000, skip: () => process.platform !== 'darwin' },
+  async (ctx) => {
+    let firstSessionId: string | undefined;
 
-  await ctx
-    .includeSessionEnvelopes()
-    .ignoreExpectationOrder()
-    .expect({
-      envelope: (envelope: Envelope) => {
-        const session = getSessionFromEnvelope(envelope);
-        firstSessionId = session?.sid;
-        expect(firstSessionId).toBeDefined();
+    await ctx
+      .includeSessionEnvelopes()
+      .ignoreExpectationOrder()
+      .expect({
+        envelope: (envelope: Envelope) => {
+          const session = getSessionFromEnvelope(envelope);
+          firstSessionId = session?.sid;
+          expect(firstSessionId).toBeDefined();
 
-        expect(
-          sessionEnvelope({
-            sid: UUID_MATCHER,
-            did: 'some_user',
-            init: true,
-            started: ISO_DATE_MATCHER,
-            timestamp: ISO_DATE_MATCHER,
-            status: 'ok',
-            errors: 0,
-            duration: expect.any(Number),
-            attrs: expect.objectContaining({
-              environment: 'development',
-              release: 'session-abnormal-exit-electron-uploader@1.0.0',
+          expect(
+            sessionEnvelope({
+              sid: UUID_MATCHER,
+              did: 'some_user',
+              init: true,
+              started: ISO_DATE_MATCHER,
+              timestamp: ISO_DATE_MATCHER,
+              status: 'ok',
+              errors: 0,
+              duration: expect.any(Number),
+              attrs: expect.objectContaining({
+                environment: 'development',
+                release: 'session-abnormal-exit-electron-uploader@1.0.0',
+              }),
             }),
-          }),
-        ).toEqual(envelope);
-      },
-    })
-    .expect({
-      envelope: (envelope: Envelope) => {
-        const session = getSessionFromEnvelope(envelope);
-        expect(session?.sid).toEqual(firstSessionId);
-
-        expect(
-          sessionEnvelope({
-            sid: UUID_MATCHER,
-            did: 'some_user',
-            init: false,
-            started: ISO_DATE_MATCHER,
-            timestamp: ISO_DATE_MATCHER,
-            status: 'abnormal',
-            errors: 1,
-            duration: expect.any(Number),
-            attrs: expect.objectContaining({
-              environment: 'development',
-              release: 'session-abnormal-exit-electron-uploader@1.0.0',
-            }),
-          }),
-        ).toEqual(envelope);
-      },
-    })
-    .expect({
-      envelope: sessionEnvelope(
-        expect.objectContaining({
+          ).toEqual(envelope);
+        },
+      })
+      .expect({
+        envelope: sessionEnvelope({
           sid: UUID_MATCHER,
           did: 'some_user',
+          init: true,
           started: ISO_DATE_MATCHER,
           timestamp: ISO_DATE_MATCHER,
+          status: 'ok',
+          errors: 0,
           duration: expect.any(Number),
           attrs: expect.objectContaining({
             environment: 'development',
             release: 'session-abnormal-exit-electron-uploader@1.0.0',
           }),
         }),
-      ),
-    })
-    .run();
-});
+      })
+      .expect({
+        envelope: (envelope: Envelope) => {
+          const session = getSessionFromEnvelope(envelope);
+          expect(session?.sid).toEqual(firstSessionId);
+
+          expect(
+            sessionEnvelope({
+              sid: UUID_MATCHER,
+              did: 'some_user',
+              init: false,
+              started: ISO_DATE_MATCHER,
+              timestamp: ISO_DATE_MATCHER,
+              status: 'abnormal',
+              errors: 1,
+              duration: expect.any(Number),
+              attrs: expect.objectContaining({
+                environment: 'development',
+                release: 'session-abnormal-exit-electron-uploader@1.0.0',
+              }),
+            }),
+          ).toEqual(envelope);
+        },
+      })
+      .run();
+  },
+);
--- test/e2e/test-apps/sessions/abnormal-exit/test.ts
+++ test/e2e/test-apps/sessions/abnormal-exit/test.ts
@@ -2,53 +2,58 @@ import { expect } from 'vitest';
 
 import { electronTestRunner, ISO_DATE_MATCHER, sessionEnvelope, UUID_MATCHER } from '../../..';
 
-electronTestRunner(__dirname, { runTwice: true, timeout: 25_000 }, async (ctx) => {
-  await ctx
-    .includeSessionEnvelopes()
-    .ignoreExpectationOrder()
-    .expect({
-      envelope: sessionEnvelope({
-        sid: UUID_MATCHER,
-        init: true,
-        started: ISO_DATE_MATCHER,
-        timestamp: ISO_DATE_MATCHER,
-        status: 'ok',
-        errors: 0,
-        duration: expect.any(Number),
-        attrs: expect.objectContaining({
-          environment: 'development',
-          release: 'session-abnormal-exit@1.0.0',
-        }),
-      }),
-    })
-    .expect({
-      envelope: sessionEnvelope({
-        sid: UUID_MATCHER,
-        init: false,
-        started: ISO_DATE_MATCHER,
-        timestamp: ISO_DATE_MATCHER,
-        status: 'abnormal',
-        errors: 1,
-        duration: expect.any(Number),
-        attrs: expect.objectContaining({
-          environment: 'development',
-          release: 'session-abnormal-exit@1.0.0',
-        }),
-      }),
-    })
-    .expect({
-      envelope: sessionEnvelope(
-        expect.objectContaining({
+electronTestRunner(
+  __dirname,
+  { runTwice: true, timeout: 25_000, skip: () => process.platform !== 'darwin' },
+  async (ctx) => {
+    await ctx
+      .includeSessionEnvelopes()
+      .ignoreExpectationOrder()
+      .expect({
+        envelope: sessionEnvelope({
           sid: UUID_MATCHER,
+          init: true,
           started: ISO_DATE_MATCHER,
           timestamp: ISO_DATE_MATCHER,
+          status: 'ok',
+          errors: 0,
           duration: expect.any(Number),
           attrs: expect.objectContaining({
             environment: 'development',
             release: 'session-abnormal-exit@1.0.0',
           }),
         }),
-      ),
-    })
-    .run();
-});
+      })
+      .expect({
+        envelope: sessionEnvelope({
+          sid: UUID_MATCHER,
+          init: true,
+          started: ISO_DATE_MATCHER,
+          timestamp: ISO_DATE_MATCHER,
+          status: 'ok',
+          errors: 0,
+          duration: expect.any(Number),
+          attrs: expect.objectContaining({
+            environment: 'development',
+            release: 'session-abnormal-exit@1.0.0',
+          }),
+        }),
+      })
+      .expect({
+        envelope: sessionEnvelope({
+          sid: UUID_MATCHER,
+          init: false,
+          started: ISO_DATE_MATCHER,
+          timestamp: ISO_DATE_MATCHER,
+          status: 'abnormal',
+          errors: 1,
+          duration: expect.any(Number),
+          attrs: expect.objectContaining({
+            environment: 'development',
+            release: 'session-abnormal-exit@1.0.0',
+          }),
+        }),
+      })
+      .run();
+  },
+);
