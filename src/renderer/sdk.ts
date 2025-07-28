4e5ccf5802ba33c8a20242007d8f3a9da0696975
Tim Fish <tim@timfish.uk>
2025-07-28 14:04:44 +0200

    feat: Move renderer event loop block detection to an integration

--- src/renderer/sdk.ts
+++ src/renderer/sdk.ts
@@ -1,3 +1,4 @@
+/* eslint-disable deprecation/deprecation */
 /* eslint-disable no-restricted-globals */
 import {
   BrowserOptions,
@@ -6,7 +7,7 @@ import {
 } from '@sentry/browser';
 import { debug, Integration } from '@sentry/core';
 import { RendererProcessAnrOptions } from '../common/ipc';
-import { enableAnrRendererMessages } from './anr';
+import { eventLoopBlockIntegration } from './integrations/event-loop-block';
 import { scopeToMainIntegration } from './integrations/scope-to-main';
 import { electronRendererStackParser } from './stack-parse';
 import { makeRendererTransport } from './transport';
@@ -21,17 +22,17 @@ export function getDefaultIntegrations(options: ElectronRendererOptions): Integr
 
 interface ElectronRendererOptions extends Omit<BrowserOptions, 'dsn' | 'environment' | 'release'> {
   /**
-   * Enables ANR detection in this renderer process.
+   * @deprecated Use `eventLoopBlockIntegration` instead.
    *
-   * Optionally accepts an object of options to configure ANR detection.
-   *
-   * {
-   *   pollInterval: number; // Defaults to 1000ms
-   *   anrThreshold: number; // Defaults to 5000ms
-   *   captureStackTrace: boolean; // Defaults to false
-   * }
-   *
-   * Defaults to 'false'.
+   * ```
+   * import * as Sentry from '@sentry/electron/renderer';
+   * Sentry.init({
+   *   dsn: '__YOUR_DSN__',
+   *   integrations: [
+   *     Sentry.eventLoopBlockIntegration({ threshold: 1000 }),
+   *   ],
+   * });
+   * ```
    */
   anrDetection?: Partial<RendererProcessAnrOptions> | boolean;
 
@@ -86,8 +87,30 @@ If init has been called in the preload and contextIsolation is disabled, is not 
     options.transport = makeRendererTransport;
   }
 
+  // TODO: Next major version, remove the deprecated anrDetection option
   if (options.anrDetection) {
-    enableAnrRendererMessages(options.anrDetection === true ? {} : options.anrDetection);
+    const integrationOptions =
+      options.anrDetection === true
+        ? {
+            threshold: 5_000,
+            pollInterval: 1_000,
+            captureStackTrace: false,
+          }
+        : {
+            threshold: options.anrDetection.anrThreshold || 5_000,
+            pollInterval: options.anrDetection.pollInterval || 1_000,
+            captureStackTrace: options.anrDetection.captureStackTrace || false,
+          };
+
+    const integration = eventLoopBlockIntegration(integrationOptions);
+
+    if (typeof options.integrations === 'function') {
+      const originalFn = options.integrations;
+      options.integrations = (integrations) => [...originalFn(integrations), integration];
+    } else {
+      options.integrations = options.integrations || [];
+      options.integrations.push(integration);
+    }
   }
 
   // We only handle initialScope in the main process otherwise it can cause race conditions over IPC
