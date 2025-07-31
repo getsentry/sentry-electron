be3d95c8aec282970be3be8d6e97b2cdeae97ccf
Tim Fish <tim@timfish.uk>
2025-07-31 16:08:45 +0200

    feat: Capture logs for Electron integrations

--- src/main/integrations/child-process.ts
+++ src/main/integrations/child-process.ts
@@ -1,5 +1,12 @@
-import { addBreadcrumb, captureMessage, defineIntegration, SeverityLevel } from '@sentry/core';
-import { childProcessIntegration as nodeChildProcessIntegration, NodeClient } from '@sentry/node';
+import {
+  addBreadcrumb,
+  captureMessage,
+  defineIntegration,
+  Log,
+  ParameterizedString,
+  SeverityLevel,
+} from '@sentry/core';
+import { childProcessIntegration as nodeChildProcessIntegration, logger, NodeClient } from '@sentry/node';
 import { app } from 'electron';
 import { EXIT_REASONS, ExitReason } from '../electron-normalize';
 import { ElectronMainOptions } from '../sdk';
@@ -22,21 +29,26 @@ const DEFAULT_OPTIONS: ChildProcessOptions = {
   events: ['abnormal-exit', 'launch-failed', 'integrity-failure'],
 };
 
+type LogFn = (msg: ParameterizedString, attributes: Log['attributes']) => void;
+
 /** Gets message and severity */
-function getMessageAndSeverity(reason: ExitReason, proc?: string): { message: string; level: SeverityLevel } {
-  const message = `'${proc}' process exited with '${reason}'`;
+function getMessageAndSeverity(
+  reason: ExitReason,
+  process: string,
+): { message: ParameterizedString; level: SeverityLevel; log: LogFn } {
+  const message = logger.fmt`'${process}' process exited with '${reason}'`;
 
   switch (reason) {
     case 'abnormal-exit':
     case 'killed':
-      return { message, level: 'warning' };
+      return { message, level: 'warning', log: logger.warn };
     case 'crashed':
     case 'oom':
     case 'launch-failed':
     case 'integrity-failure':
-      return { message, level: 'fatal' };
+      return { message, level: 'fatal', log: logger.error };
     default:
-      return { message, level: 'debug' };
+      return { message, level: 'debug', log: logger.info };
   }
 }
 
@@ -79,11 +91,19 @@ export const childProcessIntegration = defineIntegration((userOptions: Partial<O
 
           // And then add breadcrumbs for subsequent events
           if (breadcrumbs.includes(reason)) {
+            const { message, level, log } = getMessageAndSeverity(details.reason, details.type);
             addBreadcrumb({
               type: 'process',
               category: 'child-process',
-              ...getMessageAndSeverity(details.reason, details.type),
+              message,
+              level,
               data: details,
+            });
+
+            log(message, {
+              exitCode: details.exitCode,
+              name: details.name,
+              serviceName: details.serviceName,
             });
           }
         });
@@ -91,10 +111,10 @@ export const childProcessIntegration = defineIntegration((userOptions: Partial<O
         app.on('render-process-gone', (_, contents, details) => {
           const { reason } = details;
           const name = clientOptions?.getRendererName?.(contents) || 'renderer';
+          const { message, level, log } = getMessageAndSeverity(details.reason, name);
 
           // Capture message first
           if (events.includes(reason)) {
-            const { message, level } = getMessageAndSeverity(details.reason, name);
             captureMessage(message, level);
           }
 
@@ -105,6 +125,10 @@ export const childProcessIntegration = defineIntegration((userOptions: Partial<O
               category: 'child-process',
               ...getMessageAndSeverity(details.reason, name),
               data: details,
+            });
+
+            log(message, {
+              exitCode: details.exitCode,
             });
           }
         });
