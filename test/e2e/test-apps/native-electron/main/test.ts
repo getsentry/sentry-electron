3ed4fc2796fa3c5970eaf3bff492344d2b651dd5
Tim Fish <tim@timfish.uk>
2025-08-14 13:40:38 +0100

    Lint

--- test/e2e/test-apps/native-electron/main/test.ts
+++ test/e2e/test-apps/native-electron/main/test.ts
@@ -22,7 +22,7 @@ electronTestRunner(__dirname, async (ctx) => {
               },
             ],
             version: SDK_VERSION,
-            integrations: undefined
+            integrations: undefined,
           },
           level: 'fatal',
           platform: 'native',
